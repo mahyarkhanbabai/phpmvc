7cfdc7ee888eead6bfc418139816c57b6befbac4
Mahyar Khanbabai <mkhanbabayy@yahoo.com>
2022-11-24 11:41:43 +0330

    feat(modelBase): implement modelBase class

---
+++ lib/modelBase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Mahyar Khanbabai (khanbabai.ir)
+ * Date: 11/24/2022
+ * Time: 11:38 AM
+ */
+class modelBase
+{
+	protected DB $db;
+	function __construct(){
+		$this->db=DB::getInstance();
+	}
+}
